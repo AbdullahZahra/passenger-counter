7f8b825de7a1ef251e308893f7d3aa3b3efd03c6
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-10-14 21:34:49 +0200

    Saving the counter

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 let count = 0
+let message = "You counted "
 
-function increment()
-{
+function increment() {
     count++
+    document.getElementById("count-el").innerText = count
+}
+
+function save() {
+    if (message == "You counted ") {
+        message += count
+    }
+    else {
+        message += ", " + count
+    }
+
+    document.getElementById("message").innerText = message
+
+    count = 0
     document.getElementById("count-el").innerText = count
 }
